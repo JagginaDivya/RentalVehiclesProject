50cf7c4874b61f4fbd3bdfbbbf5fbbabaa5a16ac
JagginaDivya <jagginaonline@gmail.com>
2022-11-03 23:52:29 +0530

    Update RentalVehicleApplication.java

--- RentalVehicle/src/main/java/com/vehicle/rental/RentalVehicleApplication.java
+++ RentalVehicle/src/main/java/com/vehicle/rental/RentalVehicleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @SpringBootApplication
 public class RentalVehicleApplication {
 
@@ -13,23 +13,5 @@ public class RentalVehicleApplication {
 		SpringApplication.run(RentalVehicleApplication.class, args);
 	}
 	
-	@GetMapping("/hello")
-	public String Hello() {
-		return "hello";
-	}
-	
-	@GetMapping("/bye")
-	public String Bye() {
-		return "Bye";
-	}
-	
-	@GetMapping("/morning")
-	public String Morning() {
-		return "Morning";
-	}
-	@GetMapping("/morning")
-	public String goodMorning() {
-		return "Morning";
-	}
 	
 }
