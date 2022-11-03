5b2aa4334d5a85c6fbf495f17504a3b4d9f7224f
sharukahmed <sharukahmed@sharuks-mbp.dlink>
2022-11-03 22:40:16 +0530

    Added Hello Api

--- RentalVehicle/src/main/java/com/vehicle/rental/RentalVehicleApplication.java
+++ RentalVehicle/src/main/java/com/vehicle/rental/RentalVehicleApplication.java
@@ -2,12 +2,21 @@ package com.vehicle.rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class RentalVehicleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentalVehicleApplication.class, args);
 	}
-
+	
+	@GetMapping("/hello")
+	public String Hello() {
+		return "hello";
+	}
+	
+	
 }
